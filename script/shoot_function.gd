4651895b8b410d1f8405f61fed60b9cb9eba468c
zeemongteng <zeemongteng@gmail.com>
2025-10-08 00:29:48 +0700

    Add parry mechanic with cooldown to player and weapon
    Implemented a parry mechanic for both the player and weapon components, including a cooldown system to prevent repeated use. Updated scene and script files to support enabling/disabling the parry hitbox and triggering parry animations. Adjusted animation timing and structure to accommodate the new parry window.

--- script/shoot_function.gd
+++ script/shoot_function.gd
@@ -1,6 +1,11 @@
 extends Node
+
 @export var bullet_scene: PackedScene
 @onready var marker_3d: Marker3D = $"../Camera3D/Marker3D"
+@export var parryBox : CollisionShape3D
+
+@export var parry_cooldown: float = 2.0  # seconds before you can parry again
+var can_parry: bool = true
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("left_mouse_button"):
@@ -14,4 +19,17 @@ func shoot():
 	get_tree().current_scene.add_child(bullet)
 
 func parry():
-	pass
+	if not can_parry:
+		print("Parry on cooldown!") 
+		return
+
+	can_parry = false
+	parryBox.disabled = false
+
+	# how long the parry window stays active
+	await get_tree().create_timer(0.3).timeout
+	parryBox.disabled = true
+
+	# cooldown time before next parry
+	await get_tree().create_timer(parry_cooldown).timeout
+	can_parry = true
--- script/weapon_component.gd
+++ script/weapon_component.gd
@@ -1,11 +1,24 @@
 extends Node3D
 
+@export var parry_cooldown: float = 2.0  # seconds
+var can_parry: bool = true
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("left_mouse_button"):
 		shoot()
+	if Input.is_action_just_pressed("right_mouse_button"):
+		parry()
 
 func shoot():
 	$AnimationPlayer.play("shoot")
 
 func parry():
-	$AnimationPlayer.play("parry")
+	if not can_parry:
+		return  
+	
+	can_parry = false
+	$AnimationPlayer3.play("parry")
+	
+
+	await get_tree().create_timer(parry_cooldown).timeout
+	can_parry = true
